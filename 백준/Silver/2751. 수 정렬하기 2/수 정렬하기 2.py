1afdcb8381847df37e38970f001a24e4f077d754
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-07-22 13:25:06 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 5132 ms, Memory: 94548 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+numbers = [int(input()) for _ in range(n)]
+
+
+def quick_sort(start, end):
+    if start == end:
+        return
+    mid = (start + end) // 2
+    quick_sort(start, mid)
+    quick_sort(mid + 1, end)
+    p1 = start
+    p2 = mid + 1
+    temp = []
+    while p1 <= mid and p2 <= end:
+        if numbers[p1] <= numbers[p2]:
+            temp.append(numbers[p1])
+            p1 += 1
+        else:
+            temp.append(numbers[p2])
+            p2 += 1
+    while p1 <= mid:
+        temp.append(numbers[p1])
+        p1 += 1
+    while p2 <= end:
+        temp.append(numbers[p2])
+        p2 += 1
+    for i in range(end - start + 1):
+        numbers[start + i] = temp[i]
+    return
+
+
+quick_sort(0, n - 1)
+print(*numbers, sep='\n')
