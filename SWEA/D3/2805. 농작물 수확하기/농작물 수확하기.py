92252069d90e45126d362ced4818e4b387284aae
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-07-23 19:39:39 +0900

    [D3] Title: 농작물 수확하기, Time: 178 ms, Memory: 51,020 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,11 +1,13 @@
 for tc in range(1, int(input()) + 1):
     n = int(input())
-    farm = [list(map(int, list(input()))) for _ in range(n)]
     ans = 0
-    for i in range(n // 2):
-        for j in range(n // 2 - i, n // 2 + i + 1):
-            ans += farm[i][j]
-    for i in range(n // 2, n):
-        for j in range(i - n // 2, (n * 3) // 2 - i):
-            ans += farm[i][j]
+    half = n // 2
+    for i in range(half):
+        line = input()
+        for j in range(half - i, half + i + 1):
+            ans += int(line[j])
+    for i in range(half + 1):
+        line = input()
+        for j in range(i, n - i):
+            ans += int(line[j])
     print(f'#{tc} {ans}')
