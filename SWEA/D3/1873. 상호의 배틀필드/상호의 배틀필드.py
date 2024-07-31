9b132aee373841c8f6f7eb3217f4721ae3ebda40
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-07-31 20:47:27 +0900

    [D3] Title: 상호의 배틀필드, Time: 221 ms, Memory: 51,784 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.py
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.py
@@ -1,8 +1,17 @@
+# 3회차 제출
+
+# 시뮬레이션 + 구현을 시키는 대로 잘 수행하면 된다.
+# 방향에 관해 반복적으로 사용하는 것들은 0,1,2,3 순서를 맞춰서 tuple, string, dictionary로 만들었다.
+# 반복적으로 사용되는 기능들은 함수로 만들었다.
+# 변수명이나 함수명을 의미가 잘 통하도록 만들거나, 주석을 달아서 가독성을 높이도록 노력하였다.
+
+
 t = int(input())
 
 direction = ((-1, 0), (0, 1), (1, 0), (0, -1))  # 0위 1오른 2아래 3왼쪽 방향
 
-tank_state = ['^', '>', 'v', '<']
+tank_state = {'^': 0, '>': 1, 'v': 2, '<': 3}
+tank_state_rev = '^>v<'
 
 move_command = {'U': 0, 'R': 1, 'D': 2, 'L': 3}
 
@@ -14,7 +23,7 @@ def find_tank():
                 global cr, cc, cd
                 cr = i
                 cc = j
-                cd = tank_state.index(board[i][j])
+                cd = tank_state[board[i][j]]
                 board[i][j] = '.'  # 끝날 때까지 굳이 탱크 위치를 board를 변경시키면서 표시할 필요 없음
                 return
 
@@ -48,7 +57,7 @@ for tc in range(1, t + 1):
             if board[cr + dr][cc + dc] == '.':  # 평지면 이동
                 cr += dr
                 cc += dc
-    board[cr][cc] = tank_state[cd]  # 다 돌리고 나서 탱크 위치에 보는 방향대로 바꿔주기
+    board[cr][cc] = tank_state_rev[cd]  # 다 돌리고 나서 게임판의 탱크 위치를 보는 방향대로 바꿔주기
     board.pop()  # 경계 탈출 방지용 맨 마지막 행 삭제
     print(f'#{tc} ', end='')
     for line in board:
