5251da0844a4f95e1cd40fd7af0a541efd3ef19d
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-01 17:41:07 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 111692 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,24 +3,25 @@ if n >= k:
     print(n - k)
 else:
     visited = [False] * 100001
-    visited[n] = True
-    now = [n]
+    visited[k] = True
+    now = [k]
     step = 0
-    while not visited[k]:
+    while not visited[n]:
         next_step = []
         for position in now:
             minus1 = position - 1
             plus1 = position + 1
-            mul2 = position << 1
             if 0 <= minus1 <= 100000 and not visited[minus1]:
                 visited[minus1] = True
                 next_step.append(minus1)
             if 0 <= plus1 <= 100000 and not visited[plus1]:
                 visited[plus1] = True
                 next_step.append(plus1)
-            if 0 <= mul2 <= 100000 and not visited[mul2]:
-                visited[mul2] = True
-                next_step.append(mul2)
+            if not position & 1:
+                div2 = position >> 1
+                if 0 <= div2 <= 100000 and not visited[div2]:
+                    visited[div2] = True
+                    next_step.append(div2)
         now = next_step
         step += 1
     print(step)
