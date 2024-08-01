82fcbc23ace4aa0eb7d27f1160d0b1bc315555f6
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-01 15:51:33 +0900

    [Silver I] Title: 안전 영역, Time: 304 ms, Memory: 116312 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,39 +1,50 @@
 import sys
 
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
 n = int(input())
-height = [list(map(int, input().split())) for _ in range(n)]
-direction = ((0, 1), (1, 0), (0, -1), (-1, 0))
+height = list(map(int, input().split())) + [0]
+MIN = MAX = height[0]
+for i in range(1, n):
+    if MIN > height[i]:
+        MIN = height[i]
+    elif MAX < height[i]:
+        MAX = height[i]
+for _ in range(n - 1):
+    for num in list(map(int, input().split())):
+        if MIN > num:
+            MIN = num
+        elif MAX < num:
+            MAX = num
+        height.append(num)
+    height.append(0)
+for _ in range(n):
+    height.append(0)
 
-MIN = MAX = height[0][0]
-for i in range(n):
-    for j in range(n):
-        if MIN > height[i][j]:
-            MIN = height[i][j]
-        elif MAX < height[i][j]:
-            MAX = height[i][j]
-
-
-def dfs(ci, cj, depth):
-    visited[ci][cj] = True
-    for di, dj in direction:
-        ni = ci + di
-        nj = cj + dj
-        if 0 <= ni < n and 0 <= nj < n and height[ni][nj] > depth and not visited[ni][nj]:
-            dfs(ni, nj, depth)
-
-
+n += 1
 ans = 1
+not_sunken = {i for i in range(n * n - n) if (i + 1) % n and height[i] > MIN}
 for rain in range(MIN, MAX):
+    will_not_sink = set()
     island = 0
-    visited = [[False] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if height[i][j] > rain and not visited[i][j]:
-                island += 1
-                dfs(i, j, rain)
+    while not_sunken:
+        now = not_sunken.pop()
+        if height[now] > rain + 1:
+            will_not_sink.add(now)
+        lands = [now]
+        while lands:
+            adj_land = []
+            for land in lands:
+                for adj in (land + 1, land - 1, land + n, land - n):
+                    if adj in not_sunken:
+                        adj_land.append(adj)
+                        not_sunken.remove(adj)
+                        if height[adj] > rain + 1:
+                            will_not_sink.add(adj)
+            lands = adj_land
+        island += 1
     if island > ans:
         ans = island
+    not_sunken = will_not_sink
+
 print(ans)
