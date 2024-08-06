74642461d1130bfeebe406d479dc90bbedd9a79a
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-06 20:39:26 +0900

    [Silver II] Title: 영재의 시험, Time: 276 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.py
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.py
@@ -2,24 +2,30 @@ answer = list(map(int, input().split()))
 for i in range(10):
     answer[i] -= 1
 correct_cnt = [[1 if j == answer[i] else 0 for j in range(5)] for i in range(10)]
-third_choice = [[[num for num in range(5) if not num == i == j] for j in range(5)] for i in range(5)]
+# 먼저 푼 코드에서 함수를 이용해서, 5문제를 이미 맞은 경우에 대한 경우의 수를 구해 하드코딩해두었다.
+already_got5_same = [0, 0, 0, 0, 0, 2240, 464, 96, 20, 4, 1]  # 마지막 2개가 같은 경우
+already_got5_diff = [0, 0, 0, 0, 0, 2704, 560, 116, 24, 5, 1]  # 마지막 2개가 다른 경우
+ans = 0
 
 
 def guess(idx, correct, before2, before1):
-    if idx == 10:
-        if correct > 4:
-            return 1
-        return 0
     if correct + 10 - idx < 5:
-        return 0
-    cnt = 0
-    for choice in third_choice[before2][before1]:
-        cnt += guess(idx + 1, correct + correct_cnt[idx][choice], before1, choice)
-    return cnt
+        return
+    if correct == 5:
+        global ans
+        if before2 == before1:
+            ans += already_got5_same[idx]
+            return
+        ans += already_got5_diff[idx]
+        return
+    for choice in range(5):
+        if before2 == before1 == choice:
+            continue
+        guess(idx + 1, correct + correct_cnt[idx][choice], before1, choice)
+    return
 
 
-ans = 0
 for first in range(5):
     for second in range(5):
-        ans += guess(2, correct_cnt[0][first] + correct_cnt[1][second], first, second)
+        guess(2, correct_cnt[0][first] + correct_cnt[1][second], first, second)
 print(ans)
