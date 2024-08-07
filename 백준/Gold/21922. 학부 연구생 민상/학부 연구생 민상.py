4d25ac2851cf8ae1b1697f0ea69db90a9ade4a68
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-07 14:14:26 +0900

    [Gold V] Title: 학부 연구생 민상, Time: 3020 ms, Memory: 589720 KB -BaekjoonHub

---
+++ 백준/Gold/21922. 학부 연구생 민상/학부 연구생 민상.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+lab = [list(map(int, input().split())) for _ in range(n)]
+for line in lab:
+    line.append(5)
+lab.append([5] * m)
+
+# 0:우, 1:하, 2:좌, 3:상 순서대로
+di = (0, 1, 0, -1)
+dj = (1, 0, -1, 0)
+# 0이나 9이면 방향 그대로, 1,2,3,4면 방향 전환
+turnabout = ((0, 1, 2, 3), (2, 1, 0, 3), (0, 3, 2, 1), (3, 2, 1, 0), (1, 0, 3, 2), (), (), (), (), (0, 1, 2, 3))
+# visited[i][j][d]는 i행 j열 d방향을 방문했는지 여부
+visited = [[[False] * 4 for _ in range(m)] for __ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        if lab[i][j] == 9:
+            for d in range(4):
+                ci = i
+                cj = j
+                cd = d
+                while True:
+                    num = lab[ci][cj]
+                    if num == 5:
+                        break
+                    if visited[ci][cj][cd]:
+                        break
+                    visited[ci][cj][cd] = True
+                    cd = turnabout[num][cd]
+                    ci += di[cd]
+                    cj += dj[cd]
+
+ans = sum(any(visited[i][j]) for i in range(n) for j in range(m))
+print(ans)
