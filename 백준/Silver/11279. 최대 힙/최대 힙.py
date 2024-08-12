f1b7129056cec8d798ca07153679ec77c74a7d14
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-12 09:29:57 +0900

    [Silver II] Title: 최대 힙, Time: 148 ms, Memory: 116916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,17 @@
+import os, io, heapq
+
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+n = int(input())
+priority_queue = []
+ans = []
+for i in range(n):
+    x = int(input())
+    if x:
+        heapq.heappush(priority_queue, -x)
+    else:
+        if priority_queue:
+            ans.append(-heapq.heappop(priority_queue))
+        else:
+            ans.append(0)
+print(*ans, sep='\n')
