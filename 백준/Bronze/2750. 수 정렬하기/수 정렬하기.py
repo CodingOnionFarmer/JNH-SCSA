d57e782d2b744c7f01b1ff33e91e2cddf0a88929
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-12 13:37:27 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 110972 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,39 +1,28 @@
-# 병합 정렬 (Merge Sort)
+# 퀵 정렬 (Quick Sort)
+# index 기반으로 원본 리스트를 변화시키며 정렬하는 방식
 
 n = int(input())
 numbers = [int(input()) for _ in range(n)]
 
 
-def merge_sort(start, end):  # 인덱스 start부터 end까지 재귀적으로 병합정렬한다.
-    if start == end:  # 종료 조건
+def quick_sort(start, end):  # start 인덱스부터 end 인덱스까지 퀵 정렬
+    if start >= end:  # 종료 조건
         return
-    mid = (start + end) // 2
-    merge_sort(start, mid)  # 하부 호출(왼쪽)
-    merge_sort(mid + 1, end)  # 하부 호출(오른쪽)
-    
-    # 하부 호출을 한 뒤, 그 결과물을 이용해서 단위 동작
-    # 정렬된 상태의 두 리스트를 병합하여 O(n)에 정렬한다.
-    # 병합은 약 logn번 이루어지므로, 병합 정렬의 전체 시간복잡도는 O(nlogn)이다.
-    p1 = start
-    p2 = mid + 1
-    temp = []
-    while p1 <= mid and p2 <= end:
-        if numbers[p1] <= numbers[p2]:
-            temp.append(numbers[p1])
-            p1 += 1
-        else:
-            temp.append(numbers[p2])
-            p2 += 1
-    while p1 <= mid:
-        temp.append(numbers[p1])
-        p1 += 1
-    while p2 <= end:
-        temp.append(numbers[p2])
-        p2 += 1
-    for i in range(end - start + 1):
-        numbers[start + i] = temp[i]
-    return
+    pivot = numbers[start]  # 단위 동작
+    p = start
+    q = end
+    while p < q:  # pivot보다 작거나 같은 건 왼쪽, 큰 건 오른쪽에 두면서 중간에서 만나기 기법
+        while numbers[p] <= pivot and p < q:
+            p += 1
+        while numbers[q] > pivot:
+            q -= 1
+        if p < q:
+            numbers[p], numbers[q] = numbers[q], numbers[p]
+    numbers[start] = numbers[q]
+    numbers[q] = pivot  # pivot을 기준으로 더 왼쪽에 있으면 작거나 같은 것, 오른쪽에 있으면 큰 것이다.
+    quick_sort(start, q - 1)  # 하부 호출
+    quick_sort(q + 1, end)
 
 
-merge_sort(0, n - 1)
+quick_sort(0, n - 1)
 print(*numbers, sep='\n')
