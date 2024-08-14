ae90dc8b38b5142ba738be5b913e6594ff257a07
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-14 20:24:28 +0900

    [Silver I] Title: Z, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,8 +1,8 @@
 def z(r, c):
     if r < 2 and c < 2:
-        return (r << 1) + c
-    return (z(r >> 1, c >> 1) << 2) + z(r & 1, c & 1)
+        return ((r & 1) << 1) + (c & 1)
+    return (z(r >> 1, c >> 1) << 2) + ((r & 1) << 1) + (c & 1)
 
 
-n, r, c = map(int, input().split())
-print(z(r, c))
+n, R, C = map(int, input().split())
+print(z(R, C))
