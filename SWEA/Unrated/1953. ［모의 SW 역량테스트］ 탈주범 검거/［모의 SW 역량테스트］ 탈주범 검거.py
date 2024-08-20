38564487d303c65d421d9965f18412d0d050b512
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-20 20:13:47 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 169 ms, Memory: 49,692 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
@@ -1,7 +1,16 @@
+# 언어 : PyPy3
+# 메모리 : 51792KB
+# 실행시간 : 194ms
+# 시도횟수 : 1회
+
+# BFS, 시뮬레이션, 구현
+
+# 룩업 테이블을 만들어두고 두 칸에 파이프가 있고 서로 연결되어있는지 판정했다.
+
+
 directions = ((0, 1), (1, 0), (0, -1), (-1, 0))  # 우 하 좌 상
-pipes = (
-    (), (True, True, True, True), (False, True, False, True), (True, False, True, False), (True, False, False, True),
-    (True, True, False, False), (False, True, True, False), (False, False, True, True))
+pipes = ((), (0, 1, 2, 3), (1, 3), (0, 2), (0, 3), (0, 1), (1, 2), (2, 3))
+can_move_from_direction = [{1, 3, 6, 7}, {1, 2, 4, 7}, {1, 3, 4, 5}, {1, 2, 5, 6}]
 
 T = int(input())
 for tc in range(1, T + 1):
@@ -9,20 +18,18 @@ for tc in range(1, T + 1):
     tunnel = [list(map(int, input().split())) + [0] for _ in range(n)] + [[0] * m]
     now = {r * m + c}
     ans = 0
-    while now and l:
+    while now and l:  # 시간이 다 되거나 더 갈 곳이 없을 때까지
         move = set()
         for position in now:
             ci, cj = position // m, position % m
             p = tunnel[ci][cj]
             tunnel[ci][cj] = 0
             ans += 1
-            for d in range(4):  # 우0 하1 좌2 상3, d의 반대방향은 (d+2)%4이다.
-                if pipes[p][d]:
-                    di, dj = directions[d]
-                    ni, nj = ci + di, cj + dj
-                    np = tunnel[ni][nj]
-                    if np and pipes[np][(d + 2) % 4]:
-                        move.add(ni * m + nj)
+            for d in pipes[p]:
+                di, dj = directions[d]
+                ni, nj = ci + di, cj + dj
+                if tunnel[ni][nj] in can_move_from_direction[d]:
+                    move.add(ni * m + nj)
         now = move
         l -= 1
     print(f'#{tc} {ans}')
