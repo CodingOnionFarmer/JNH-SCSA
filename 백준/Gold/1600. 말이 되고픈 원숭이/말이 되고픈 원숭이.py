dd4e5a9b090d158d29936749c156a9f422abcaba
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-26 20:05:17 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 364 ms, Memory: 131500 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,32 +1,41 @@
+# bfs, 완전탐색, 구현
+# 방문 처리 관련 아이디어 추가
+
+
 k = int(input())
 w, h = map(int, input().split())
 trip = [list(map(int, input().split())) + [1, 1] for _ in range(h)] + [[1] * (w + 2) for _ in range(2)]
+
+# 상하좌우 이동
 directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+# 나이트 이동
 knight_directions = ((1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2))
-visited = [[[False] * (k + 1) for __ in range(w)] for _ in range(h)]
-visited[0][0][0] = True
+
+# visited[i][j]는 i,j 칸에 해당 숫자만큼의 찬스를 쓰고 갈 수 있음을 의미한다
+visited = [[31] * w for _ in range(h)]
+visited[0][0] = 0
 q = [(0, 0, 0)]  # 찬스 쓴 개수, 행위치, 열위치
-chance = k
 step = 0
-while q and not any(visited[h - 1][w - 1][i] for i in range(k + 1)):
+while q and visited[h - 1][w - 1] > k:
     step += 1
     nq = []
     for used, ci, cj in q:
         for di, dj in directions:
             ni, nj = ci + di, cj + dj
-            if trip[ni][nj] or visited[ni][nj][used]:
+            if trip[ni][nj] or visited[ni][nj] <= used:
                 continue
-            visited[ni][nj][used] = True
+            visited[ni][nj] = used
             nq.append((used, ni, nj))
         if used < k:
             for di, dj in knight_directions:
                 ni, nj = ci + di, cj + dj
-                if trip[ni][nj] or visited[ni][nj][used + 1]:
+                if trip[ni][nj] or visited[ni][nj] <= used + 1:
                     continue
-                visited[ni][nj][used + 1] = True
+                visited[ni][nj] = used + 1
                 nq.append((used + 1, ni, nj))
     q = nq
-if any(visited[h - 1][w - 1][i] for i in range(k + 1)):
+if visited[h - 1][w - 1] <= k:
     print(step)
 else:
     print(-1)
