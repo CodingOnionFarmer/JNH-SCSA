6f57826316ae39c103cad62ad06bde14ba2db4d9
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-27 17:47:25 +0900

    [Gold IV] Title: 지뢰찾기, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/2140. 지뢰찾기/지뢰찾기.py
@@ -0,0 +1,39 @@
+n = int(input())
+if n < 3:
+    print(0)
+elif n == 3:
+    top_line = int(input())
+    if top_line:
+        print(1)
+    else:
+        print(0)
+else:
+    top_line = list(map(int, list(input())))
+    leftmost = top_line[0]
+    rightmost = top_line[-1]
+    mine = leftmost + rightmost + (n - 4) ** 2
+    left = leftmost
+    left_left = 0
+    for j in range(1, n - 3):
+        left_left, left = left, top_line[j] - left - left_left
+        mine += left
+    leftmost_up = 0
+    rightmost_up = 0
+    for i in range(1, n - 3):
+        ith_line = input()
+        leftmost_up, leftmost = leftmost, int(ith_line[0]) - leftmost - leftmost_up
+        mine += leftmost
+        rightmost_up, rightmost = rightmost, int(ith_line[-1]) - rightmost - rightmost_up
+        mine += rightmost
+    input()
+    input()
+    bottom_line = list(map(int, list(input())))
+    leftmost = bottom_line[0]
+    rightmost = bottom_line[-1]
+    mine += leftmost + rightmost
+    left = leftmost
+    left_left = 0
+    for j in range(1, n - 3):
+        left_left, left = left, bottom_line[j] - left - left_left
+        mine += left
+    print(mine)
