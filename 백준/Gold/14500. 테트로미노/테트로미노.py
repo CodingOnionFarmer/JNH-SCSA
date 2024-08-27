ed752edf08abe41a8cab3a8f0b10b543847dd39d
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-27 18:51:32 +0900

    [Gold IV] Title: 테트로미노, Time: 144 ms, Memory: 112968 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,57 +1,79 @@
-"""
-시작 시간 : 3시 01분
-제출 시간 : 3시 23분
-
-"""
-
-# tetra_minos = set()
-# directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
-# visited = set()
-#
-#
-# def build_mino(depth, blocks):
-#     if depth == 4:
-#         tetra_minos.add(blocks)
-#         return
-#     if blocks in visited:
-#         return
-#     visited.add(blocks)
-#     adj = set()
-#     for ci, cj in blocks:
-#         for di, dj in directions:
-#             ni, nj = ci + di, cj + dj
-#             if ni < 0 or (not ni and nj <= 0) or (ni, nj) in blocks:
-#                 continue
-#             adj.add((ni, nj))
-#     for ni, nj in adj:
-#         build_mino(depth + 1, tuple(sorted(list(blocks + ((ni, nj),)))))
-#
-#
-# build_mino(1, ((0, 0),))
-# print(tetra_minos)
-# print(len(tetra_minos))
-#
-# tetra_minos = tuple([mino[1:] for mino in tetra_minos])
-# print(tetra_minos)
-
-# 검증 완료
-
-tetra_minos = (
-    ((0, 1), (1, 1), (1, 2)), ((0, 1), (0, 2), (0, 3)), ((1, 0), (1, 1), (2, 1)), ((0, 1), (0, 2), (1, 0)),
-    ((1, -1), (1, 0), (1, 1)), ((0, 1), (1, 0), (1, 1)), ((1, 0), (2, -1), (2, 0)), ((0, 1), (0, 2), (1, 1)),
-    ((1, 0), (1, 1), (1, 2)), ((1, 0), (2, 0), (2, 1)), ((0, 1), (0, 2), (1, 2)), ((1, -1), (1, 0), (2, -1)),
-    ((1, -2), (1, -1), (1, 0)), ((1, 0), (2, 0), (3, 0)), ((1, 0), (1, 1), (2, 0)),
-    ((1, -1), (1, 0), (2, 0)), ((0, 1), (1, -1), (1, 0)), ((0, 1), (1, 1), (2, 1)), ((0, 1), (1, 0), (2, 0))
-)
+import sys
 
 n, m = map(int, input().split())
-board = [list(map(int, input().split())) + [0, 0, 0] for _ in range(n)] + [[0] * (m + 3) for _ in range(3)]
-most = 0
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+score = 0
 for i in range(n):
-    for j in range(m):
-        temp = board[i][j]
-        for (di1, dj1), (di2, dj2), (di3, dj3) in tetra_minos:
-            temp_sum = temp + board[i + di1][j + dj1] + board[i + di2][j + dj2] + board[i + di3][j + dj3]
-            if temp_sum > most:
-                most = temp_sum
-print(most)
+    s = paper[i][0] + paper[i][1] + paper[i][2]
+    for j in range(3, m):
+        s += paper[i][j]
+        if s > score:
+            score = s
+        s -= paper[i][j - 3]
+for i in range(m):
+    t = paper[0][i] + paper[1][i] + paper[2][i]
+    for j in range(3, n):
+        t += paper[j][i]
+        if t > score:
+            score = t
+        t -= paper[j - 3][i]
+for i in range(n - 1):
+    for j in range(m - 1):
+        s = paper[i][j] + paper[i][j + 1] + paper[i + 1][j] + paper[i + 1][j + 1]
+        if s > score:
+            score = s
+for i in range(n - 1):
+    for j in range(m - 2):
+        r = [paper[i][j], paper[i][j + 1], paper[i][j + 2], paper[i + 1][j], paper[i + 1][j + 1], paper[i + 1][j + 2]]
+        s = r[0] + r[1] + r[2] + r[3]
+        if s > score:
+            score = s
+        s += r[4] - r[3]
+        if s > score:
+            score = s
+        s += r[5] - r[4]
+        if s > score:
+            score = s
+        s += r[4] - r[2]
+        if s > score:
+            score = s
+        s += r[3] - r[1]
+        if s > score:
+            score = s
+        s += r[1] - r[0]
+        if s > score:
+            score = s
+        s += r[2] - r[1]
+        if s > score:
+            score = s
+        s += r[1] - r[5]
+        if s > score:
+            score = s
+for i in range(m - 1):
+    for j in range(n - 2):
+        r = [paper[j][i], paper[j + 1][i], paper[j + 2][i], paper[j][i + 1], paper[j + 1][i + 1], paper[j + 2][i + 1]]
+        s = r[0] + r[1] + r[2] + r[3]
+        if s > score:
+            score = s
+        s += r[4] - r[3]
+        if s > score:
+            score = s
+        s += r[5] - r[4]
+        if s > score:
+            score = s
+        s += r[4] - r[2]
+        if s > score:
+            score = s
+        s += r[3] - r[1]
+        if s > score:
+            score = s
+        s += r[1] - r[0]
+        if s > score:
+            score = s
+        s += r[2] - r[1]
+        if s > score:
+            score = s
+        s += r[1] - r[5]
+        if s > score:
+            score = s
+print(score)
