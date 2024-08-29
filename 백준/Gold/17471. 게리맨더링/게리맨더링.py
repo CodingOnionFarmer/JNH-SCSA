2827c69e786578a51720c0dbe6089e27deac1394
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-08-29 20:46:05 +0900

    [Gold III] Title: 게리맨더링, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.py
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.py
@@ -1,34 +1,8 @@
-# 언어 : PyPy3
-# 메모리 : 111364KB
-# 시간 : 124ms
-# 시도횟수 : 1회
-
-# BFS, DFS, 백트래킹, 완전탐색, 그래프 탐색
-
-"""
-1. 비트연산을 활용하여, 2^n - 2가지(한 쪽이 원소가 0개인 경우 제외)의 그룹 나누기 경우의 수를 완전탐색한다.
-    -> 비트가 0인 쪽과 1인 쪽 두 그룹으로 나누는 것이다.
-
-2. 두 그룹의 인구수 차이가 현재 최소치보다 크거나 같으면 판정할 필요 없이 가지치기한다.
-
-3. 최소치 갱신할 가능성이 있으면, 두 그룹이 각각 하나의 연결된 집합인지 판정한다.
-    -> 이를 판정하기 위해, 각 그룹에서 대표 원소 아무거나 하나씩 뽑고, BFS 순회로 방문 가능한 모든 점을 방문처리한다.
-    -> 방문처리도 비트마스킹을 활용했다.
-    -> 두 그룹의 대표 원소에서 출발해서 순회 한 번씩을 끝마치고 난 뒤, 미방문 점이 없으면 두 그룹 다 연결되어 있는 것이다.
-
-4. 최소치를 갱신했는데 그게 1 이하이면 뒤에 더 볼 필요 없이 종료한다.
-"""
-
-### 실행시간 단축을 위해 몇 가지 로직 변경 ###
-
-# 지역구의 수
 n = int(input())
 
-# 각 지역구의 인구수와 총 인구수
 popularity = list(map(int, input().split()))
 total = sum(popularity)
 
-# graph[i]에 j가 있으면, i와 j가 연결된 것
 graph = [0] * n
 for i in range(n):
     lst = list(map(int, input().split()))
@@ -36,23 +10,17 @@ for i in range(n):
         for j in lst[1:]:
             graph[i] |= 1 << j - 1
 
-# 방문처리를 비트마스킹으로 하고, 그게 full = 111...1(1이 n개)이면, 모든 점을 방문한 것이다.
 full = (1 << n) - 1
 best = 1001
 
-# 대칭성이 있으므로, 마지막 지역구는 그룹0으로 고정해도 됨 -> 범위 조정
 for code in range(1, 1 << n - 1):
     code0 = full - code
-    # code가 0111011이면, 2,6번 지역구가 그룹0, 0,1,3,4,5번 지역구가 그룹1이다.
-    # 두 그룹의 인구수를 다 셀 필요는 없다(총 인구수 total을 세어 놨으므로)
     group1_popularity = sum(popularity[i] for i in range(n) if (1 << i) & code)
     diff = abs(total - (group1_popularity << 1))
 
-    # 최소 기록 갱신 불가능한 경우는 가지치기
     if diff >= best:
         continue
 
-    # 그룹0 bfs 순회하면서 방문처리
     q = 1 << n - 1
     visited = 0
     while q:
@@ -68,7 +36,6 @@ for code in range(1, 1 << n - 1):
     if visited != code0:
         continue
 
-    # 그룹1 bfs 순회하면서 방문처리
     q = 1
     while not q & code:
         q <<= 1
@@ -82,15 +49,11 @@ for code in range(1, 1 << n - 1):
         nq &= full - visited
         q = nq
 
-    # 모든 점을 방문했어야, 각 그룹이 잘 연결된 상태이다.
     if visited == full:
-        # 갱신 가능한 경우에만 탐색했으므로, 반드시 갱신
         best = diff
-        # 현재 최소값이 더 갱신 불가능한 값이면 조기 종료
         if best < 2:
             break
 
-# 초기값(불가능한 값)이 전혀 갱신되지 않았으면 두 연결된 그룹으로 나누는 경우가 아예 없는 것이다.
 if best == 1001:
     best = -1
 print(best)
