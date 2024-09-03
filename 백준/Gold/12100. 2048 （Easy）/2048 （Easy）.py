08c613f11f51edb84bd9e0d65e74a12716249735
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-09-03 12:55:13 +0900

    [Gold II] Title: 2048 (Easy), Time: 236 ms, Memory: 113036 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,21 +1,17 @@
-"""
-BOJ : 2048(Easy)
-
-시작 시간 : 3시 45분
-구상 완료 : 3시 47분
-제출 시간 : 4시 06분
-
-"""
-
 n = int(input())
 original_board = [list(map(int, input().split())) for _ in range(n)]
-biggest = max(max(line) for line in original_board)
+original_biggest = max(max(line) for line in original_board)
+biggest = original_biggest
 
 # 방향 : 오른,밑,왼,위로 밀기
 
 for move_code in range(1024):
     board = [line[:] for line in original_board]
-    for _ in range(5):
+    temp_biggest = original_biggest
+    for turn in range(5):
+        if temp_biggest << (5-turn) == biggest:
+            break
+
         move = move_code % 4
         move_code >>= 2
 
@@ -30,8 +26,8 @@ for move_code in range(1024):
                     if num == before:
                         moved_line_from_right[-1] <<= 1
                         before = -1
-                        if num == biggest:
-                            biggest <<= 1
+                        if num == temp_biggest:
+                            temp_biggest <<= 1
                     else:
                         moved_line_from_right.append(num)
                         before = num
@@ -50,8 +46,8 @@ for move_code in range(1024):
                     if num == before:
                         moved_line_from_bottom[-1] <<= 1
                         before = -1
-                        if num == biggest:
-                            biggest <<= 1
+                        if num == temp_biggest:
+                            temp_biggest <<= 1
                     else:
                         moved_line_from_bottom.append(num)
                         before = num
@@ -70,8 +66,8 @@ for move_code in range(1024):
                     if num == before:
                         moved_line_from_left[-1] <<= 1
                         before = -1
-                        if num == biggest:
-                            biggest <<= 1
+                        if num == temp_biggest:
+                            temp_biggest <<= 1
                     else:
                         moved_line_from_left.append(num)
                         before = num
@@ -90,13 +86,16 @@ for move_code in range(1024):
                     if num == before:
                         moved_line_from_top[-1] <<= 1
                         before = -1
-                        if num == biggest:
-                            biggest <<= 1
+                        if num == temp_biggest:
+                            temp_biggest <<= 1
                     else:
                         moved_line_from_top.append(num)
                         before = num
                     board[i][j] = 0
                 for idx, num in enumerate(moved_line_from_top):
                     board[idx][j] = num
+                    
+    if temp_biggest > biggest:
+        biggest = temp_biggest
 
 print(biggest)
