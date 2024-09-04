14843e625a2b9acfdca5dc0d3b4ec8dcd28ed8f2
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-09-04 18:55:52 +0900

    [Gold III] Title: 나무 재테크, Time: 272 ms, Memory: 112848 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -8,46 +8,53 @@ BOJ : 나무 재테크
 
 """
 
+n, m, k = map(int, input().split())
+
 # directions = tuple((i, j) for i in range(-1, 2) for j in range(-1, 2) if i or j)
 # print(directions)
 directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+adj = tuple(
+    tuple(tuple((i + di, j + dj) for di, dj in directions if 0 <= i + di < n and 0 <= j + dj < n) for j in range(n))
+    for i in range(n)
+)
 
-n, m, k = map(int, input().split())
 soil = [[5] * n for _ in range(n)]
 trees = [[[] for __ in range(n)] for _ in range(n)]
+baby_trees = [[0] * n for _ in range(n)]
 
 winter_soil = [list(map(int, input().split())) for _ in range(n)]
-for tree in range(m):
+for _ in range(m):
     x, y, z = map(int, input().split())
     trees[x - 1][y - 1].append(z)
-for i in range(n):
-    for j in range(n):
-        trees[i][j].sort()
 
 for year in range(k):
     for i in range(n):
         for j in range(n):
-            if trees[i][j]:
-                for idx, age in enumerate(trees[i][j]):
-                    if age <= soil[i][j]:
-                        soil[i][j] -= age
-                        trees[i][j][idx] += 1
-                    else:
-                        dead = 0
-                        for old in range(idx, len(trees[i][j])):
-                            dead += trees[i][j].pop() >> 1
-                        soil[i][j] += dead
-                        break
+            baby = baby_trees[i][j]
+            tree = trees[i][j]
+            s = soil[i][j]
+            if baby >= s:
+                trees[i][j] = [2] * s
+                soil[i][j] = sum(t >> 1 for t in tree)
+                continue
+            s -= baby
+            trees[i][j] = [2] * baby
+            dead = 0
+            for t in tree:
+                if t <= s:
+                    s -= t
+                    trees[i][j].append(t + 1)
+                else:
+                    dead += t >> 1
+            soil[i][j] = s + dead
 
+    baby_trees = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if trees[i][j]:
-                for age in trees[i][j]:
-                    if not age % 5:
-                        for di, dj in directions:
-                            ni, nj = i + di, j + dj
-                            if 0 <= ni < n and 0 <= nj < n:
-                                trees[ni][nj].insert(0, 1)
+            for age in trees[i][j]:
+                if not age % 5:
+                    for ni, nj in adj[i][j]:
+                        baby_trees[ni][nj] += 1
             soil[i][j] += winter_soil[i][j]
 
-print(sum(sum(len(trees[i][j]) for i in range(n)) for j in range(n)))
+print(sum(sum(len(trees[i][j]) for i in range(n)) for j in range(n)) + sum(sum(line) for line in baby_trees))
