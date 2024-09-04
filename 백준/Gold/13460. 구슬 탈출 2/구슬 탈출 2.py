54e94d9aee569a98e38bf7209f49db693b5c145d
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-09-04 14:46:38 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -38,199 +38,103 @@ visited[ri][rj][bi][bj] = True
 
 
 def solve():
-    q = [(ri, rj, bi, bj, -1)]
+    q = [(ri, rj, bi, bj)]
     step = 0
-    nq = []
-    step += 1
-    for rx, ry, bx, by, d in q:
-        if ry >= by:
-            # 오른쪽 기울이기
-            red_goal_in, nrx, nry = move(0, rx, ry, bx, by)
-            blue_goal_in, nbx, nby = move(0, bx, by, nrx, nry)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 0))
-
-            # 왼쪽 기울이기
-            blue_goal_in, nbx, nby = move(2, bx, by, rx, ry)
-            red_goal_in, nrx, nry = move(2, rx, ry, nbx, nby)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 2))
-
-        else:
-            # 오른쪽 기울이기
-            blue_goal_in, nbx, nby = move(0, bx, by, rx, ry)
-            red_goal_in, nrx, nry = move(0, rx, ry, nbx, nby)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 0))
-
-            # 왼쪽 기울이기
-            red_goal_in, nrx, nry = move(2, rx, ry, bx, by)
-            blue_goal_in, nbx, nby = move(2, bx, by, nrx, nry)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 2))
-
-        if rx >= bx:
-            # 아래쪽 기울이기
-            red_goal_in, nrx, nry = move(1, rx, ry, bx, by)
-            blue_goal_in, nbx, nby = move(1, bx, by, nrx, nry)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 1))
-
-            # 위쪽 기울이기
-            blue_goal_in, nbx, nby = move(3, bx, by, rx, ry)
-            red_goal_in, nrx, nry = move(3, rx, ry, nbx, nby)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 3))
-
-        else:
-            # 아래쪽 기울이기
-            blue_goal_in, nbx, nby = move(1, bx, by, rx, ry)
-            red_goal_in, nrx, nry = move(1, rx, ry, nbx, nby)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 1))
-
-            # 위쪽 기울이기
-            red_goal_in, nrx, nry = move(3, rx, ry, bx, by)
-            blue_goal_in, nbx, nby = move(3, bx, by, nrx, nry)
-            if not blue_goal_in:
-                if red_goal_in:
-                    print(step)
-                    return
-                if not visited[nrx][nry][nbx][nby]:
-                    visited[nrx][nry][nbx][nby] = True
-                    nq.append((nrx, nry, nbx, nby, 3))
-    q = nq
     while q and step < 10:
         nq = []
         step += 1
-        for rx, ry, bx, by, d in q:
-            if d & 1:
-                if ry >= by:
-                    # 오른쪽 기울이기
-                    red_goal_in, nrx, nry = move(0, rx, ry, bx, by)
-                    blue_goal_in, nbx, nby = move(0, bx, by, nrx, nry)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 0))
+        for rx, ry, bx, by in q:
+            if ry >= by:
+                # 오른쪽 기울이기
+                red_goal_in, nrx, nry = move(0, rx, ry, bx, by)
+                blue_goal_in, nbx, nby = move(0, bx, by, nrx, nry)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
 
-                    # 왼쪽 기울이기
-                    blue_goal_in, nbx, nby = move(2, bx, by, rx, ry)
-                    red_goal_in, nrx, nry = move(2, rx, ry, nbx, nby)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 2))
+                # 왼쪽 기울이기
+                blue_goal_in, nbx, nby = move(2, bx, by, rx, ry)
+                red_goal_in, nrx, nry = move(2, rx, ry, nbx, nby)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
 
-                else:
-                    # 오른쪽 기울이기
-                    blue_goal_in, nbx, nby = move(0, bx, by, rx, ry)
-                    red_goal_in, nrx, nry = move(0, rx, ry, nbx, nby)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 0))
-
-                    # 왼쪽 기울이기
-                    red_goal_in, nrx, nry = move(2, rx, ry, bx, by)
-                    blue_goal_in, nbx, nby = move(2, bx, by, nrx, nry)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 2))
             else:
-                if rx >= bx:
-                    # 아래쪽 기울이기
-                    red_goal_in, nrx, nry = move(1, rx, ry, bx, by)
-                    blue_goal_in, nbx, nby = move(1, bx, by, nrx, nry)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 1))
+                # 오른쪽 기울이기
+                blue_goal_in, nbx, nby = move(0, bx, by, rx, ry)
+                red_goal_in, nrx, nry = move(0, rx, ry, nbx, nby)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
 
-                    # 위쪽 기울이기
-                    blue_goal_in, nbx, nby = move(3, bx, by, rx, ry)
-                    red_goal_in, nrx, nry = move(3, rx, ry, nbx, nby)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 3))
+                # 왼쪽 기울이기
+                red_goal_in, nrx, nry = move(2, rx, ry, bx, by)
+                blue_goal_in, nbx, nby = move(2, bx, by, nrx, nry)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
 
-                else:
-                    # 아래쪽 기울이기
-                    blue_goal_in, nbx, nby = move(1, bx, by, rx, ry)
-                    red_goal_in, nrx, nry = move(1, rx, ry, nbx, nby)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 1))
+            if rx >= bx:
+                # 아래쪽 기울이기
+                red_goal_in, nrx, nry = move(1, rx, ry, bx, by)
+                blue_goal_in, nbx, nby = move(1, bx, by, nrx, nry)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
 
-                    # 위쪽 기울이기
-                    red_goal_in, nrx, nry = move(3, rx, ry, bx, by)
-                    blue_goal_in, nbx, nby = move(3, bx, by, nrx, nry)
-                    if not blue_goal_in:
-                        if red_goal_in:
-                            print(step)
-                            return
-                        if not visited[nrx][nry][nbx][nby]:
-                            visited[nrx][nry][nbx][nby] = True
-                            nq.append((nrx, nry, nbx, nby, 3))
+                # 위쪽 기울이기
+                blue_goal_in, nbx, nby = move(3, bx, by, rx, ry)
+                red_goal_in, nrx, nry = move(3, rx, ry, nbx, nby)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
+
+            else:
+                # 아래쪽 기울이기
+                blue_goal_in, nbx, nby = move(1, bx, by, rx, ry)
+                red_goal_in, nrx, nry = move(1, rx, ry, nbx, nby)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
+
+                # 위쪽 기울이기
+                red_goal_in, nrx, nry = move(3, rx, ry, bx, by)
+                blue_goal_in, nbx, nby = move(3, bx, by, nrx, nry)
+                if not blue_goal_in:
+                    if red_goal_in:
+                        print(step)
+                        return
+                    if not visited[nrx][nry][nbx][nby]:
+                        visited[nrx][nry][nbx][nby] = True
+                        nq.append((nrx, nry, nbx, nby))
         q = nq
 
     print(-1)
