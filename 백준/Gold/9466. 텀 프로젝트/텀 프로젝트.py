03e7ef812c957c63ec9cb783228dbe43156f85ec
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-09-07 14:10:30 +0900

    [Gold III] Title: 텀 프로젝트, Time: 908 ms, Memory: 242024 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,26 +1,23 @@
 T = int(input())
 for tc in range(1, T + 1):
-    n = int(input())
-    want_to_be_group = list(map(int, input().split()))
-    visited = [False] * n
-    representative = [i for i in range(n)]
+    n = int(input()) + 1
+    want_to_be_group = [0] + list(map(int, input().split()))
+    representative = [0] * n
     depth = [0] * n
     students_in_team = 0
-    for i in range(n):
-        if visited[i]:
+    for i in range(1,n):
+        if representative[i]:
             continue
-        visited[i] = True
-        r = i
-        now = r
+        representative[i] = i
+        now = i
         size = 1
         while True:
-            now = want_to_be_group[now] - 1
-            if visited[now]:
-                if representative[now] == r:
+            now = want_to_be_group[now]
+            if representative[now]:
+                if representative[now] == i:
                     students_in_team += size - depth[now]
                 break
-            visited[now] = True
-            representative[now] = r
+            representative[now] = i
             depth[now] = size
             size += 1
-    print(n - students_in_team)
+    print(n - students_in_team - 1)
