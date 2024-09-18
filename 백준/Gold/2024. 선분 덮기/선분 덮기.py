8ab7c89d456ee9c70e2edf7ae902ea15537f43ed
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-09-18 14:28:22 +0900

    [Gold III] Title: 선분 덮기, Time: 512 ms, Memory: 116884 KB -BaekjoonHub

---
+++ 백준/Gold/2024. 선분 덮기/선분 덮기.py
@@ -0,0 +1,36 @@
+m = int(input())
+lines = []
+while True:
+    l, r = map(int, input().split())
+    if not l and not r:
+        break
+    lines.append((l, r))
+n = len(lines)
+lines.sort()
+
+
+def solve():
+    end = 0
+    idx = 0
+    ans = 0
+    while idx < n and end < m:
+        new_end = end
+        new_idx = idx
+        while new_idx < n and lines[new_idx][0] <= end:
+            if lines[new_idx][1] >= new_end:
+                new_end = lines[new_idx][1]
+            new_idx += 1
+        if new_idx == idx or new_end == end:
+            print(0)
+            return
+        end = new_end
+        idx += 1
+        ans += 1
+    if end >= m:
+        print(ans)
+        return
+    print(0)
+    return
+
+
+solve()
