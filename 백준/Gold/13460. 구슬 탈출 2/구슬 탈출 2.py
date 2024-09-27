1cd1a52dcf8a561f5fb03913449b2729487b9d76
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-09-27 16:35:01 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,144 +1,141 @@
-"""
-BOJ : 구슬 탈출 2
-
-시작 시간 : 3시 00분
-구상 완료 : 3시 08분
-제출 시간 : 3시 37분
-"""
-
-# 우 하 좌 상
-directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
-
 n, m = map(int, input().split())
-board = [list(input()) for _ in range(n)]
-visited = [[[[False] * m for _ in range(n)] for __ in range(m)] for ___ in range(n)]
-ri, rj, bi, bj, gi, gj = 0, 0, 0, 0, 0, 0
-for i in range(1, n - 1):
-    for j in range(1, m - 1):
-        if board[i][j] == 'R':
-            ri, rj = i, j
-        elif board[i][j] == 'B':
-            bi, bj = i, j
-        elif board[i][j] == 'O':
-            gi, gj = i, j
+n -= 1
+m -= 1
+board = [0] * (n * m)
+input()
+rsp, bsp, ep = 0, 0, 0
 
-
-def move(d, cri, crj, cbi, cbj):
-    di, dj = directions[d]
-    while True:
-        nri, nrj = cri + di, crj + dj
-        if nri == gi and nrj == gj:
-            return True, 0, 0
-        if board[nri][nrj] == '#' or (nri == cbi and nrj == cbj):
-            return False, cri, crj
-        cri, crj = nri, nrj
-
-
-visited[ri][rj][bi][bj] = True
-
-
-def solve():
-    q = [(ri, rj, bi, bj)]
-    step = 0
-    while q and step < 10:
-        nq = []
-        step += 1
-        for rx, ry, bx, by in q:
-            if ry >= by:
-                # 오른쪽 기울이기
-                red_goal_in, nrx, nry = move(0, rx, ry, bx, by)
-                blue_goal_in, nbx, nby = move(0, bx, by, nrx, nry)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
-
-                # 왼쪽 기울이기
-                blue_goal_in, nbx, nby = move(2, bx, by, rx, ry)
-                red_goal_in, nrx, nry = move(2, rx, ry, nbx, nby)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
-
+for i in range(n):
+    line = input()
+    for j in range(m):
+        char = line[j + 1]
+        if char != '.':
+            if char == 'B':
+                bsp = i * m + j
+            elif char == 'R':
+                rsp = i * m + j
+            elif char == 'O':
+                ep = i * m + j
+                board[ep] = 2
             else:
-                # 오른쪽 기울이기
-                blue_goal_in, nbx, nby = move(0, bx, by, rx, ry)
-                red_goal_in, nrx, nry = move(0, rx, ry, nbx, nby)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
+                board[i * m + j] = 1
 
-                # 왼쪽 기울이기
-                red_goal_in, nrx, nry = move(2, rx, ry, bx, by)
-                blue_goal_in, nbx, nby = move(2, bx, by, nrx, nry)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
 
-            if rx >= bx:
-                # 아래쪽 기울이기
-                red_goal_in, nrx, nry = move(1, rx, ry, bx, by)
-                blue_goal_in, nbx, nby = move(1, bx, by, nrx, nry)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
+def move(fp, sp, d):  # d가 1:오른쪽, -1:왼쪽, m:아래쪽, -m:위쪽
+    while not board[fp]:
+        fp += d
+    if board[fp] == 1:
+        fp -= d
+        board[fp] = 1
+    while not board[sp]:
+        sp += d
+    if board[sp] == 1:
+        sp -= d
+    if board[fp] == 1:
+        board[fp] = 0
+    return fp, sp
 
-                # 위쪽 기울이기
-                blue_goal_in, nbx, nby = move(3, bx, by, rx, ry)
-                red_goal_in, nrx, nry = move(3, rx, ry, nbx, nby)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
 
+visited = [[False] * (n * m) for _ in range(n * m)]  # red blue 순
+visited[rsp][bsp] = True
+for i in range(n * m):
+    visited[i][i] = True
+    visited[i][ep] = True
+moved = 0
+goal_in = False
+q = [(rsp, bsp, 0), (rsp, bsp, 1)]  # 0은 수직 1은 수평
+
+while q and not goal_in:
+    if moved == 10:
+        break
+    moved += 1
+    nq = []
+    for rcp, bcp, cvh in q:
+        # 1은 수평 이동
+        if cvh:
+            # 빨강이 더 오른쪽
+            if rcp % m > bcp % m:
+                # 오른쪽
+                rnp, bnp = move(rcp, bcp, 1)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 0))
+
+                # 왼쪽
+                bnp, rnp = move(bcp, rcp, -1)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 0))
+
+            # 빨강이 더 왼쪽
             else:
-                # 아래쪽 기울이기
-                blue_goal_in, nbx, nby = move(1, bx, by, rx, ry)
-                red_goal_in, nrx, nry = move(1, rx, ry, nbx, nby)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
+                # 왼쪽
+                rnp, bnp = move(rcp, bcp, -1)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 0))
 
-                # 위쪽 기울이기
-                red_goal_in, nrx, nry = move(3, rx, ry, bx, by)
-                blue_goal_in, nbx, nby = move(3, bx, by, nrx, nry)
-                if not blue_goal_in:
-                    if red_goal_in:
-                        print(step)
-                        return
-                    if not visited[nrx][nry][nbx][nby]:
-                        visited[nrx][nry][nbx][nby] = True
-                        nq.append((nrx, nry, nbx, nby))
-        q = nq
+                # 오른쪽
+                bnp, rnp = move(bcp, rcp, 1)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 0))
 
-    print(-1)
-    return
+        # 0은 수직 이동
+        else:
+            # 빨강이 더 아래쪽
+            if rcp > bcp:
+                # 아래쪽
+                rnp, bnp = move(rcp, bcp, m)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 1))
 
+                # 위쪽
+                bnp, rnp = move(bcp, rcp, -m)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 1))
 
-solve()
+            # 빨강이 더 위쪽
+            else:
+                # 위쪽
+                rnp, bnp = move(rcp, bcp, -m)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 1))
+
+                # 아래쪽
+                bnp, rnp = move(bcp, rcp, m)
+                if not visited[rnp][bnp]:
+                    if rnp == ep:
+                        goal_in = True
+                        break
+                    visited[rnp][bnp] = True
+                    nq.append((rnp, bnp, 1))
+    q = nq
+
+if not goal_in:
+    moved = -1
+print(moved)
