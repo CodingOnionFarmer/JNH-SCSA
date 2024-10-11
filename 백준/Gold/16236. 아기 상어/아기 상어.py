29096d9a61d8d0ee21729d1faa39bfb9ffb77a08
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-10-11 09:11:24 +0900

    [Gold III] Title: 아기 상어, Time: 100 ms, Memory: 110616 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -4,17 +4,17 @@ n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 adj = [[[(i + di, j + dj) for di, dj in directions if 0 <= i + di < n and 0 <= j + dj < n] for j in range(n)] for i in
        range(n)]
-level = 2
-exp = 0
-time = 0
-ci, cj = 0, 0
-for i in range(n):
-    for j in range(n):
-        if board[i][j] == 9:
-            board[i][j] = 0
-            ci, cj = i, j
 
-while True:
+
+def find():
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 9:
+                board[i][j] = 0
+                return i, j
+
+
+def bfs(ci, cj, level):
     step = 0
     can_kill = []
     visited = [[False] * n for _ in range(n)]
@@ -36,14 +36,27 @@ while True:
                         can_kill.append((ni, nj))
         q = nq
     if can_kill:
-        time += step
-        ci, cj = min(can_kill)
-        board[ci][cj] = 0
-        exp += 1
-        if exp == level:
-            exp = 0
-            level += 1
-    else:
-        break
+        return step, *min(can_kill)
+    return 0, 0, 0
 
-print(time)
+
+def solve():
+    level = 2
+    exp = 0
+    time = 0
+    ci, cj = find()
+    while True:
+        distance, ci, cj = bfs(ci, cj, level)
+        if distance:
+            time += distance
+            board[ci][cj] = 0
+            exp += 1
+            if exp == level:
+                exp = 0
+                level += 1
+        else:
+            print(time)
+            return
+
+
+solve()
