5de037bbe5603cddaceb1cc88bdf06df07f77b96
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2024-11-29 11:19:14 +0900

    [Platinum IV] Title: 열혈강호, Time: 256 ms, Memory: 62092 KB -BaekjoonHub

--- 백준/Platinum/11375. 열혈강호/열혈강호.py
+++ 백준/Platinum/11375. 열혈강호/열혈강호.py
@@ -2,15 +2,8 @@
 # 홉크로프트-카프 알고리즘
 
 n, m = map(int, input().split())
-staff_level = [0] * (n + 1)
 task_matching = [0] * (m + 1)
-graph = [[]]
-graph_rev = [[] for _ in range(m + 1)]
-for s in range(1, n + 1):
-    graph.append(list(map(int, input().split()))[1:])
-    for t in graph[s]:
-        graph_rev[t].append(s)
-edges = [[False] * (m + 1) for _ in range(n + 1)]
+graph = [[]] + [list(map(int,input().split()))[1:] for _ in range(n)]
 remaining_staff = {s for s in range(1, n + 1)}
 
 
@@ -35,29 +28,6 @@ def dfs(s, level):
 
 match = 0
 while True:
-    vs = [False] * (n + 1)
-    vt = [False] * (m + 1)
-    q = [s for s in remaining_staff]
-    for s in q:
-        vs[s] = True
-    level = 0
-    while q:
-        tq = []
-        level += 1
-        for s in q:
-            for t in graph[s]:
-                if vt[t]:
-                    continue
-                vt[t] = True
-                tq.append(t)
-        q = []
-        for t in tq:
-            for s in graph_rev[t]:
-                if vs[s]:
-                    continue
-                vs[s] = True
-                staff_level[s] = level
-                q.append(s)
     vs = [False] * (n + 1)
     vt = [False] * (m + 1)
     new_vs = set()
